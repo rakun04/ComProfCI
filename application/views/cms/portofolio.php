66d375742c562e137730c7305de998192d96c1da
rakun04 <ramdhankun@gmail.com>
2018-08-29 11:22:49 +0700

    fix commit

--- application/views/cms/portofolio.php
+++ application/views/cms/portofolio.php
@@ -124,40 +124,18 @@
 	</div>
 	<?php $setting=$this->MModel->get("select * from profile where id_profile='1'");?>
 
-	<section class="service-section  w3ls-section" id="services">
+	<section class="w3ls-section w3-product-all" id="product-all">
+		<div class="container">
+			<div class="row">
+				<div class="tcb-prdct-panel">
+					<img src="" alt="">
+					<div class="tcb-prdct-body">
 
-		<!-- <div class="container">
-
-			<div class="service-grids agile">
-				<div class="service-grid agileits_w3layouts ">
-					<?php if($data){
-			foreach ($data as $p) { ?>
-					<div class="col-md-4 contact-right-grid">
-						<div class="panel-default">
-							<center>
-								<img class="img-responsive" src="<?=base_url().'img/product/'.$p['foto_product']?>">
-							</center>
-
-							<div class="panel-heading bg-default">
-								<center>
-									<a href="<?=base_url().'Welcome/detProduk/'.$p['id_product']?>">
-										<h4>
-											<?=$p['nama_product']?>
-										</h4>
-									</a>
-								</center>
-							</div>
-						</div>
 					</div>
-					<?php } }?>
-
-					<div class="clearfix"></div>
-					<hr>
-					<?=$page?>
-
 				</div>
 			</div>
-		</div> -->
+		</div>
+
 
 	</section>
 
